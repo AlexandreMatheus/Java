596c90ed82a1501644ee57d4842319817568058d
AlexandreMatheus <33810376+AlexandreMatheus@users.noreply.github.com>
2018-10-14 12:31:51 -0300

    Update arrayeprint.java

--- Exercicios-Array/arrayeprint.java
+++ Exercicios-Array/arrayeprint.java
@@ -2,18 +2,13 @@ import java.util.*;
 class Main {
   public static void main(String[] args) {
     int[] numeros = new int[300];
-    
-    for(int x=0; x<numeros.length;x++){
-      numeros[x]=35;
-    }
+    Arrays.fill(numeros, 35);
 
-    /*1º forma de print
+    /*1º forma de print 
     for(int i = 0; i<numeros.length;i++){
       System.out.println("Position: " + i + " -> " + numeros[i]);
-    }
-    */
-   
-
+    }*/
+    
     /*2º forma de print
     int cont=0;
     for(int n : numeros) {
